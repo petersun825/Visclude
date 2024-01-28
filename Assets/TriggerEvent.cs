8712dacae565943f54ebebdcb8a22eea6cb38c03
petersun825 <33887715+petersun825@users.noreply.github.com>
2024-01-28 10:36:47 -0500

    Getting things to work

--- Assets/TriggerEvent.cs
+++ Assets/TriggerEvent.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,23 +8,33 @@ public class TriggerEvent : MonoBehaviour
 {
     public string tagToCheck;
     public UnityEvent toTrigger;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject pairmentObject;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(tagToCheck))
         {
+            // Check the active state and set it to the opposite
+            bool isActive = pairmentObject.activeSelf;
+         
+            Debug.Log("activeself: " + pairmentObject.activeSelf);
+            pairmentObject.SetActive(!isActive);
+            Debug.Log("activeself: " + pairmentObject.activeSelf);
 
+            // Optionally, invoke the UnityEvent
             toTrigger.Invoke();
+        }
+    }
+
+    private void SetBackgroundColor(string hexColor)
+    {
+        Color color;
+        if (ColorUtility.TryParseHtmlString(hexColor, out color))
+        {
+            Camera.main.backgroundColor = color;
+        }
+        else
+        {
+            Debug.LogError("Invalid color string: " + hexColor);
         }
     }
 }
